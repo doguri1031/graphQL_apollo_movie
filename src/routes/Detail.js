accd0e9271832082e3a4409330cc4853250f21a6
lee <doguri1031@gmail.com>
2020-07-13 00:41:26 +0900

    Modify Home.js, Detail.js

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,2 +1,27 @@
 import React from 'react';
-export default () => 'Detail';
+import { usePrams, useParams } from 'react-router-dom';
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
+
+const GET_MOVIE = gql`
+    query getMovie($id: Int!) {
+        movie(id: $id) {
+            id
+            title
+            medium_cover_image
+            description_intro
+        }
+    }
+`;
+export default () => {
+    const { id } = useParams();
+    const { loading, data } = useQuery(GET_MOVIE, {
+        variables: { id },
+    });
+    if (loading) {
+        return 'loading...';
+    }
+    if (data && data.moive) {
+        return 'data.movie.title';
+    }
+};
